12537ff7740eeb29283a5ba5868ea9df509dc65f
Suvidha <suvidha.nakhawa@gmail.com>
2020-06-09 23:05:31 -0400

    Initial Commit

---
+++ Easy Tip Calculator/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Easy Tip Calculator
+//
+//  Created by Suvidha on 6/9/20.
+//  Copyright © 2020 Suvidha. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
